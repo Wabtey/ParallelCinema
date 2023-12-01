7b723bc9bfabdca5de60ff58a4804e7cdc1ccc8c
Wabtey <wabtey@disroot.org>
2023-12-01 17:51:07 +0100

    add room state

--- src/cinema/Room.java
+++ src/cinema/Room.java
@@ -1,9 +1,56 @@
 package cinema;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Room {
+    enum RoomState {
+        OPEN,
+        // INCLUDING THE CREDITS
+        PROJECTING,
+        EXITING,
+        CLEANING
+    }
+
+    static final int NB_SEATS = 150;
+
     int index;
+    Map<Integer, Boolean> seats = new HashMap<>();
+
+    RoomState state = RoomState.OPEN;
 
     public Room(int index) {
         this.index = index;
+        for (int i = 0; i < NB_SEATS; i++)
+            seats.put(i, true);
+    }
+
+    public RoomState getRoomState() {
+        return this.state;
+    }
+
+    public void nextRoomState() {
+        switch (state) {
+            case RoomState.OPEN:
+                this.state = RoomState.PROJECTING;
+                break;
+            case RoomState.PROJECTING:
+                this.state = RoomState.EXITING;
+                break;
+            case RoomState.EXITING:
+                this.state = RoomState.CLEANING;
+                break;
+            case RoomState.CLEANING:
+                this.state = RoomState.OPEN;
+                break;
+            default:
+                break;
+        }
+        // this.state = this.state + 1 >= RoomState.values().length ?
+        // RoomState.values()[0] : this.state.ordinal + 1;
+    }
+
+    public String toString() {
+        return "";
     }
 }
